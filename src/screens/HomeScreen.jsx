b77463c4f287936b9fd7049d57cb79bfdaf2b7c9
jaden <asadsaifi90@gmail.com>
2023-08-08 10:43:43 -0500

    modified:   package.json 	modified:   src/screens/HomeScreen.jsx 	modified:   tailwind.config.js

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { View, Text, SafeAreaView, Image, Pressable } from "react-native";
-import tw from "tailwind-react-native-classnames"
+import { View, Text, SafeAreaView, Image, Button } from "react-native";
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView style={tw`bg-white h-full flex items-center justify-center`}>
+    <SafeAreaView className="bg-white h-full flex items-center justify-center">
       <View>
       <Image
         style={
@@ -20,13 +19,19 @@ const HomeScreen = () => {
       </View>
 
       <View>
-        <Text style={tw`text-green-700 text-xl font-extrabold`}>Save your food before it expires!</Text>
+        <Text className="text-green-700 text-xl font-extrabold">Save your food before it expires!</Text>
       </View>
 
-      <View style={tw`flex justify-center m-16 p-10`}>
-      <Pressable style={tw`rounded bg-green-500 py-4 px-12`}>
-      <Text style={tw`text-xl text-white`}>Get Started</Text>
-    </Pressable>
+      <View className="flex justify-center m-16 p-10">
+        <View className="rounded bg-green-500 text-white py-2 px-10">
+        <Button
+            title="Get Started"
+            color={"#ffff"}
+          onPress={() => {
+            // Route to the next page
+          }}
+        />
+        </View>
       </View>
     </SafeAreaView>
   );
