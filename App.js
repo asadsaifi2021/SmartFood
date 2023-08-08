c37dc2793a052c38436d08fb039fa26d9d9fbf5d
jaden <asadsaifi90@gmail.com>
2023-08-07 23:11:48 -0500

    modified:   App.js 	modified:   package-lock.json 	modified:   package.json 	modified:   src/screens/HomeScreen.jsx

--- App.js
+++ App.js
@@ -6,15 +6,5 @@ import HomeScreen from "./src/screens/HomeScreen";
 const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ title: "Welcome to SmartFood" }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  return <HomeScreen />;
 }
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,11 +1,34 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, SafeAreaView, Image, Pressable } from "react-native";
+import tw from "tailwind-react-native-classnames"
 
 const HomeScreen = () => {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-center text-teal-600">This is the HomeScreen</Text>
-    </View>
+    <SafeAreaView style={tw`bg-white h-full flex items-center justify-center`}>
+      <View>
+      <Image
+        style={
+          {
+            width: 400,
+            height: 350,
+            resizeMode: 'contain',
+          }
+        }
+        source={{
+        uri: "https://www.smartfood.com/sites/smartfood.com/themes/smartfood/img/logo-footer.png?1"
+      }}/>
+      </View>
+
+      <View>
+        <Text style={tw`text-green-700 text-xl font-extrabold`}>Save your food before it expires!</Text>
+      </View>
+
+      <View style={tw`flex justify-center m-16 p-10`}>
+      <Pressable style={tw`rounded bg-green-500 py-4 px-12`}>
+      <Text style={tw`text-xl text-white`}>Get Started</Text>
+    </Pressable>
+      </View>
+    </SafeAreaView>
   );
 };
 
