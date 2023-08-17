170e74e47fec02d64eeee68b45548596532f5fe1
jaden <asadsaifi90@gmail.com>
2023-08-17 16:19:08 -0500

    modified:   package-lock.json 	modified:   package.json 	modified:   src/screens/DashboardScreen.jsx

--- src/screens/DashboardScreen.jsx
+++ src/screens/DashboardScreen.jsx
@@ -1,13 +1,24 @@
-import { useNavigation } from "@react-navigation/native";
 import React from "react";
 import { View, Text, SafeAreaView, Image, Button } from "react-native";
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 const HomeScreen = () => {
 
     return (
-        <SafeAreaView>
-            <View>
-            <Text>This is the dashboard screen</Text>
+        <SafeAreaView className="bg-white h-full">
+            <View className="p-5">
+            <Image
+        style={
+          {
+            width: 100,
+            height: 100,
+            resizeMode: 'contain',
+          }
+        }
+        source={{
+        uri: "https://www.smartfood.com/sites/smartfood.com/themes/smartfood/img/logo-footer.png?1"
+                    }} />
+                <Ionicons name="search-outline" size={32} color="black" />
             </View>
         </SafeAreaView>
   );
